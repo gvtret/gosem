aa998e5740b10e62af9af545fd040748f0415b4b
Joan Duran <jduran@circutor.com>
2021-11-12 09:02:56 +0100

    Added AARQ without cipher

--- pkg/dlms/aarq.go
+++ pkg/dlms/aarq.go
@@ -1,43 +1,140 @@
 package dlms
 
-import "bytes"
-
-const (
-	DlmsAuthenticationNone = 0
-	DlmsAuthenticationLow  = 1
+import (
+	"bytes"
+	"encoding/binary"
 )
 
-type DlmsSettings struct {
-	Authentication byte
-	Password       []byte
+type DlmsAuthentication byte
+
+// Authentication mechanism definitions
+const (
+	DlmsAuthenticationNone       DlmsAuthentication = 0 // No authentication is used.
+	DlmsAuthenticationLow        DlmsAuthentication = 1 // Low authentication is used.
+	DlmsAuthenticationHigh       DlmsAuthentication = 2 // High authentication is used.
+	DlmsAuthenticationHighMD5    DlmsAuthentication = 3 // High authentication is used. Password is hashed with MD5.
+	DlmsAuthenticationHighSHA1   DlmsAuthentication = 4 // High authentication is used. Password is hashed with SHA1.
+	DlmsAuthenticationHighGmac   DlmsAuthentication = 5 // High authentication is used. Password is hashed with GMAC.
+	DlmsAuthenticationHighSha256 DlmsAuthentication = 6 // High authentication is used. Password is hashed with SHA-256.
+	DlmsAuthenticationHighEcdsa  DlmsAuthentication = 7 // High authentication is used. Password is hashed with ECDSA.
+)
+
+type DlmsSecurity byte
+
+const (
+	DlmsSecurityNone                     DlmsSecurity = 0    // Transport security is not used.
+	DlmsSecurityAuthentication           DlmsSecurity = 0x10 // Authentication security is used.
+	DlmsSecurityEncryption               DlmsSecurity = 0x20 // Encryption security is used.
+	DlmsSecurityAuthenticationEncryption DlmsSecurity = 0x30 // Authentication and encryption security are used.
+)
+
+type Ciphering struct {
+	Security     DlmsSecurity
+	SystemTitle  []byte
+	DedicatedKey []byte
 }
 
+type Settings struct {
+	Authentication DlmsAuthentication
+	Password       []byte
+	Ciphering      Ciphering
+	MaxPduSize     int
+}
+
+// APDU types
 const (
-	TagAARQApplicationContextName     = 1
-	TagAARQCallingAPTitle             = 6
-	TagAARQSenderAcseRequirements     = 10
-	TagAARQMechanismName              = 11
-	TagAARQCallingAuthenticationValue = 12
-	TagAARQUserInformation            = 30
+	PduTypeProtocolVersion            = 0
+	PduTypeApplicationContextName     = 1
+	PduTypeCalledAPTitle              = 2
+	PduTypeCalledAEQualifier          = 3
+	PduTypeCalledAPInvocationId       = 4
+	PduTypeCalledAEInvocationId       = 5
+	PduTypeCallingAPTitle             = 6
+	PduTypeCallingAEQualifier         = 7
+	PduTypeCallingAPInvocationId      = 8
+	PduTypeCallingAEInvocationId      = 9
+	PduTypeSenderAcseRequirements     = 10
+	PduTypeMechanismName              = 11
+	PduTypeCallingAuthenticationValue = 12
+	PduTypeImplementationInformation  = 29
+	PduTypeUserInformation            = 30
 )
 
+// BER encoding enumeration values
 const (
-	BERTypeContext = 0x80
+	BERTypeContext     = 0x80
+	BERTypeApplication = 0x40
+	BERTypeConstructed = 0x20
 )
 
-func EncodeAARQ(settings *DlmsSettings) (out []byte, err error) {
+// Application context definitions
+const (
+	ApplicationContextLNNoCiphering = 1
+	ApplicationContextSNNoCiphering = 2
+	ApplicationContextLNCiphering   = 3
+	ApplicationContextSNCiphering   = 4
+)
+
+func EncodeAARQ(settings *Settings) (out []byte, err error) {
 	var buf bytes.Buffer
-	buf.WriteByte(byte(TagAARQ))
+
+	// Application Association Request
+	buf.WriteByte(BERTypeApplication | BERTypeConstructed)
+
+	// APDU length (to be filled in later)
+	buf.WriteByte(0x00)
+
+	buf.Write(generateApplicationContextName(settings))
+	buf.Write(generateAuthentication(settings))
+	buf.Write(generateUserInformation(settings))
+
+	out = buf.Bytes()
+
+	// Add length
+	out[1] = byte(len(out) - 2)
+
+	return
+}
+
+func generateApplicationContextName(settings *Settings) (out []byte) {
+	var buf bytes.Buffer
+
+	// Application context name
+	buf.WriteByte(BERTypeContext | BERTypeConstructed | PduTypeApplicationContextName)
+	buf.Write([]byte{0x09, 0x06, 0x07, 0x60, 0x85, 0x74, 0x05, 0x08, 0x01})
+	if settings.Ciphering.Security == DlmsSecurityNone || len(settings.Ciphering.SystemTitle) > 0 {
+		buf.Write([]byte{ApplicationContextLNNoCiphering})
+	} else {
+		buf.Write([]byte{ApplicationContextLNCiphering})
+	}
+
+	if len(settings.Ciphering.SystemTitle) > 0 {
+		// Add calling-AP-title
+		buf.WriteByte(BERTypeContext | BERTypeConstructed | PduTypeCallingAPTitle)
+		buf.Write([]byte{0x0A, 0x04, 0x08})
+		buf.Write(settings.Ciphering.SystemTitle)
+	}
+
+	out = buf.Bytes()
+
+	return
+}
+
+func generateAuthentication(settings *Settings) (out []byte) {
+	var buf bytes.Buffer
 
 	if settings.Authentication != DlmsAuthenticationNone {
-		buf.WriteByte(BERTypeContext | TagAARQSenderAcseRequirements)
+		// Add sender ACSE-requirements field component.
+		buf.WriteByte(BERTypeContext | PduTypeSenderAcseRequirements)
 		buf.Write([]byte{0x02, 0x07, 0x80})
 
-		buf.WriteByte(BERTypeContext | TagAARQMechanismName)
+		// Add mechanism name.
+		buf.WriteByte(BERTypeContext | PduTypeMechanismName)
 		buf.Write([]byte{0x07, 0x60, 0x85, 0x74, 0x05, 0x08, 0x02})
-		buf.WriteByte(settings.Authentication)
+		buf.WriteByte(byte(settings.Authentication))
 
-		buf.WriteByte(BERTypeContext | TagAARQCallingAuthenticationValue)
+		// Add Calling authentication information.
+		buf.WriteByte(BERTypeContext | BERTypeConstructed | PduTypeCallingAuthenticationValue)
 		buf.WriteByte(byte(2 + len(settings.Password)))
 		buf.WriteByte(0x80)
 		buf.WriteByte(byte(len(settings.Password)))
@@ -45,5 +142,50 @@ func EncodeAARQ(settings *DlmsSettings) (out []byte, err error) {
 	}
 
 	out = buf.Bytes()
+
+	return
+}
+
+func generateUserInformation(settings *Settings) (out []byte) {
+	var buf bytes.Buffer
+
+	// User information
+	buf.WriteByte(BERTypeContext | BERTypeConstructed | PduTypeUserInformation)
+	initiateRequest := getInitiateRequest(settings)
+
+	// Encrypt initiate request if security is enabled
+
+	buf.WriteByte(byte(2 + len(initiateRequest)))
+	buf.WriteByte(0x04)
+	buf.WriteByte(byte(len(initiateRequest)))
+	buf.Write(initiateRequest)
+
+	out = buf.Bytes()
+
+	return
+}
+
+func getInitiateRequest(settings *Settings) (out []byte) {
+	var buf bytes.Buffer
+
+	// Application Association Request
+	buf.WriteByte(byte(TagInitiateRequest))
+
+	if settings.Ciphering.Security == DlmsSecurityNone || len(settings.Ciphering.DedicatedKey) == 0 {
+		buf.WriteByte(0x00)
+	} else {
+		buf.WriteByte(0x01)
+		buf.WriteByte(byte(len(settings.Ciphering.DedicatedKey)))
+		buf.Write(settings.Ciphering.DedicatedKey)
+	}
+
+	buf.Write([]byte{0x00, 0x00, 0x06, 0x5F, 0x1F, 0x04, 0x00, 0x00, 0x18, 0x1F})
+
+	maxPduSize := make([]byte, 2)
+	binary.BigEndian.PutUint16(maxPduSize, uint16(settings.MaxPduSize))
+	buf.Write(maxPduSize)
+
+	out = buf.Bytes()
+
 	return
 }
